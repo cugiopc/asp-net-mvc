350997963956a15451b158288c71577d2c4a4e25
phuonghoaivo <phuonghvo@yahoo.com>
2017-07-13 20:11:39 +0700

    Add movie view and controller

---
+++ StarMovie/Models/Entity/Movie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StarMovie.Models.Entity
+{
+    public class Movie
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        /// <value>
+        /// The title.
+        /// </value>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the release date.
+        /// </summary>
+        /// <value>
+        /// The release date.
+        /// </value>
+        public DateTime ReleaseDate { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the price.
+        /// </summary>
+        /// <value>
+        /// The price.
+        /// </value>
+        public decimal Price { get; set; }
+    }
+}
---
+++ StarMovie/Models/StarMovieDbContext.cs
@@ -0,0 +1,17 @@
+﻿using StarMovie.Models.Entity;
+using System.Data.Entity;
+
+namespace StarMovie.Models
+{
+    public class StarMovieDbContext : DbContext
+    {
+        /// <summary>
+        /// Gets or sets the movies.
+        /// </summary>
+        /// <value>
+        /// The movies.
+        /// </value>
+        public DbSet<Movie> Movies { get; set; }
+
+    }
+}
