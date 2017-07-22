4f9873142328a97f1b0c566a5f88f311b761c99d
phuonghoaivo <phuonghvo@yahoo.com>
2017-07-22 16:19:35 +0700

    Update config local db

--- Web API/WebAPI.DataLayer/Migrations/Configuration.cs
+++ Web API/WebAPI.DataLayer/Migrations/Configuration.cs
@@ -7,30 +7,30 @@ namespace WebAPI.DataLayer.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(WebAPI.DataLayer.WebApiDataContext context)
         {
             //  This method will be called after migrating to the latest version.
 
-            context.Blogs.AddOrUpdate(
-              new Model.Blog
-              {
-                  AuthorId = 1,
-                  BlogId = 1,
-                  BriefDescription = "BriefDescription",
-                  CreatedDate = DateTime.Now,
-                  Description = "Description...",
-                  ImageFile = "C:\\Images",
-                  IsActive = true,
-                  IsHottest = false,
-                  IsPopular = false,
-                  Name = "Example",
-                  NumView = 1000,
-                  Tags = "AAA"
-              }
-            );
+            //context.Blogs.AddOrUpdate(
+            //  new Model.Blog
+            //  {
+            //      AuthorId = 1,
+            //      BlogId = 1,
+            //      BriefDescription = "BriefDescription",
+            //      CreatedDate = DateTime.Now,
+            //      Description = "Description...",
+            //      ImageFile = "C:\\Images",
+            //      IsActive = true,
+            //      IsHottest = false,
+            //      IsPopular = false,
+            //      Name = "Example",
+            //      NumView = 1000,
+            //      Tags = "AAA"
+            //  }
+            //);
 
         }
     }
